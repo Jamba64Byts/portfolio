02dfb76f0165d2f4b2036e87cd393abe68597ab7
NaelsonBR <32687895+NaelsonBR@users.noreply.github.com>
2017-10-12 22:32:21 -0300

    CreateButtonMenu

--- js/global.js
+++ js/global.js
@@ -1,1 +1,48 @@
+$(function () {
+    $('[data-toggle="popover"]').popover('enable');
+});
 
+var static = 0;
+
+//Dev Naelson
+
+$(function () {
+    //Modo over
+/*
+    $('.second-list > li > a > i').hover(function () {
+            $('.down').show();
+
+        },
+        function () {
+            $('.down').hover(function () {
+                }, function () {
+                    $('.down').hide();
+                }
+            )
+
+
+    });
+*/
+    var static = 0;
+    $('.second-list > li > a > i').click(function () {
+        static = static + 1;
+        console.log(static);
+        if (static === 1) {
+            $('.down').show();
+
+        } else if (static === 2) {
+            static = 0;
+            $('.down').hide();
+        }
+    });
+
+    $(document).on('scroll', function () {
+        var num = $(this).scrollTop();
+        if (num > 0) {
+            $('.down').hide();
+        } else if (num <= 0) {
+            $('.down').show();
+        }
+
+    })
+});
